5786176e854923c235dbfb88ad8bf9fde64503db
olsikule <olsi.kule@gmail.com>
2023-04-05 15:48:14 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ const Footer = () => {
     return (
         <div className='footerBar'>
             <p>
-                © In Collaboration with Olsi Kule, Sammy Lam, and Paul Pableo
+                © In Collaboration with 
+                <a href="https://sammylam.dev/" target="_blank"> Sammy Lam </a>, 
+                <a href="https://www.olsikule.ca" target="_blank"> Olsi Kule </a> & 
+                <a href="https://paulpableo.com/" target="_blank"> Paul Pableo</a>
             </p>
         </div>
     )
